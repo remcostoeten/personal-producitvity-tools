51fdd9f3021c7caa054953e25bd79e1952688cdc
remco stoeten <remcostoeten@hotmail.com>
2023-05-24 03:29:52 +0200

    Add firebase.

---
+++ lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// Import other Firebase SDKs you need
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase app
+const app = !getApp() ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+// Export the Firebase app and Firestore instance
+export { app, db };
